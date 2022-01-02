345832ed674e1a359cb20899e857fad2abcb2f7e
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-01-02 15:03:45 +0530

    Time: 4 ms (90.72%), Space: 7.7 MB (85.16%) - LeetHub

---
+++ richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) 
+    {
+        int max =-1;
+        for(int i=0;i<accounts.size();i++)
+        {
+            int s=0;
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                s=s+accounts[i][j];
+            }
+            if(s>max)
+            {
+                max=s;
+            }
+        }
+        return max;
+        
+        
+    }
+};

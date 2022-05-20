1f2e76d711e118e7a331483bac9d590630ef6c07
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-05-20 15:06:56 +0530

    Added solution - LeetHub

--- Three Great Candidates - GFG/three-great-candidates.cpp
+++ Three Great Candidates - GFG/three-great-candidates.cpp
@@ -9,25 +9,28 @@ using namespace std;
 class Solution{   
 public:
     long long maxProduct(int arr[], int n) {
-        // code here
-        if(n<3)
-       return -1;
-       sort(arr,arr+n);
-       long long p=1,r=1;
-       p=p*arr[0];
-       p=p*arr[1];
-       p=p*arr[n-1];
-       r=r*arr[n-1];
-       r=r*arr[n-2];
-       r=r*arr[n-3];
-       return max(p,r);
-        //  long long p1=1;
-        //  long long p2=1;
-        // // int p2=1;
-        // sort(arr,arr+n);
-        // p1=arr[n-1]*arr[n-2]*arr[n-3];
-        // p2=arr[0]*arr[1]*arr[n-1];
-        // return(p1>p2?p1:p2);
+        
+    //     if(n<3)
+    //   return -1;
+    //   sort(arr,arr+n);
+    //   long long p=1,r=1;
+    //   p=p*arr[0];
+    //   p=p*arr[1];
+    //   p=p*arr[n-1];
+    //   r=r*arr[n-1];
+    //   r=r*arr[n-2];
+    //   r=r*arr[n-3];
+    //   return max(p,r);
+       
+          if(n<3)
+          return -1;
+          sort(arr,arr+n);
+          long  p1=1;
+          long  p2=1;
+         p1=(long)arr[n-1]*arr[n-2]*arr[n-3];
+         p2=(long)arr[0]*arr[1]*arr[n-1];
+        //  return(p1>p2?p1:p2);
+        return max(p1,p2);
         
     }
 };

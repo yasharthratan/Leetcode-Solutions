cbfa0c053a6a7cc0665ea262605ba317193d7fc5
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-06-04 22:35:49 +0530

    Time: 3 ms (96.11%), Space: 8.5 MB (33.82%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+        /*vector<int>ans;
+        for(int i=0;i<=n;i++)
+        {
+            int c=0;
+            while(i>0)
+            {
+                int d=i%2;
+                if(d==1)
+                {
+                    c++;
+                }
+                i/=2;
+            }
+            ans.push_back(c);
+            
+        }
+        return ans;
+        */
+        vector<int> ans;
+		
+		// iterating fromt 0 to n
+        for(int i = 0; i<=n; i++)
+        {
+			// sum is initialised as 0
+            int sum = 0;
+            int num = i;
+			// while num not equals zero
+            while(num != 0)
+            {
+				// we have to count 1's in binary representation of i, therefore % 2
+                sum += num%2;
+                num = num/2;
+            }
+			// add sum to ans vector
+            ans.push_back(sum);
+        }
+		// return 
+        return ans;
+    }
+};

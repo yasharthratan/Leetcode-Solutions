f82f5a7573a284ae5ba98c86ffb720d0f3344450
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-06-07 23:34:59 +0530

    Added solution - LeetHub

---
+++ Check if a Integer is power of 8 or not - GFG/check-if-a-integer-is-power-of-8-or-not.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+public:
+    string is_power_of_eight(long long int n ) 
+    {
+        // Code here.
+        if(n==1)
+        {
+            return "Yes";
+        }
+        
+        while(n%8==0)
+        {
+            n/=8;
+        }
+        if(n==1)
+        {
+            return "Yes";
+        }
+        else
+        {
+            return "No";
+        }
+    }
+};
+
+
+// { Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int  n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_power_of_eight(n);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends

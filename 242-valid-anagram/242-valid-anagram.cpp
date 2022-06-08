fa0dd2a461fc7992805fba3407da09ba97579d32
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-06-08 16:18:14 +0530

    Time: 3 ms (98.44%), Space: 7.2 MB (94.58%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,19 +2,26 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        sort(s.begin(),s.end());
-        
-        sort(t.begin(),t.end());
-        
-        if(s==t)
-        {
-            return true;
-        }
-        else
+        const int CHAR=256;
+    
+    if(s.length()!=t.length())
+    {
+        return false;
+    }
+    int count[CHAR]={0};
+    for(int i=0;i<s.length();i++)
+    {
+        count[s[i]]++;
+        count[t[i]]--;
+    }
+    for(int i=0;i<CHAR;i++)
+    {
+        if(count[i]!=0)
         {
             return false;
         }
-        
-        
+    }
+    return true;
+    
     }
 };

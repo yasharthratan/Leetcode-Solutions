d21ea13762d66b4a15adf008c14e4a6a9aea6122
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-06-13 17:44:23 +0530

    Time: 12 ms (33.57%), Space: 10.3 MB (53.46%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
     {
-        int c=0;
-    int N=1e6+1;//(10^6)
-        vector<int> arr(N,0);
-    vector<int> ans;
-    for(int i=0;i<nums1.size();i++)
+        int n=nums1.size();
+        int m=nums2.size();
+        vector<int>ans;
+        unordered_set<int>s;
+            for(int i=0;i<n;i++)
+            {
+                s.insert(nums1[i]);
+            }
+       
+    int res=0;
+    for(int i=0;i<m;i++)
     {
-        arr[nums1[i]]=1;
-    }
-    for(int j=0;j<nums2.size();j++)
-    {
-        if(arr[nums2[j]]==1)
-            arr[nums2[j]]=2;
-    }
-    for(int k=0;k<N;k++)
-    {
-        if(arr[k]==2)
+        if(s.find(nums2[i])!=s.end())
         {
-            ans.push_back(k);
+            res++;
+            s.erase(nums2[i]);
+            ans.push_back(nums2[i]);
         }
     }
     return ans;

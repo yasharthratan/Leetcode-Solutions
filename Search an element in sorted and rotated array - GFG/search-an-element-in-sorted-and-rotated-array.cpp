d071cf025da88dd7a9bf5e73f0852e31fcc4df50
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-06-23 19:06:46 +0530

    Added solution - LeetHub

---
+++ Search an element in sorted and rotated array - GFG/search-an-element-in-sorted-and-rotated-array.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int Search(vector<int> ,int );
+
+//User code will be pasted here
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        
+        vector<int> vec(n);
+        
+        for(int i =0;i<n;i++) 
+            cin >> vec[i];
+        
+        int target;
+        cin >> target;
+        
+        cout << Search(vec,target) << "\n";
+         
+    }
+}// } Driver Code Ends
+
+
+//User function template for C++
+
+// vec : given vector of elements
+// K : given value whose index we need to find 
+int Search(vector<int> vec, int K) {
+    
+    int j=-1;
+    int n=vec.size();
+    for(int i=0;i<n;i++)
+    {
+        if(vec[i]==K)
+        {
+            j=i;
+            break;
+        }
+    }
+    return j;
+    //code here
+}

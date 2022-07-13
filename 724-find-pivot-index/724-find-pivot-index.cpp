b30f2ce92e7172f6e32996ee81d42af812a40cb1
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-07-13 22:57:32 +0530

    Time: 29 ms (80.49%), Space: 31.2 MB (54.28%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,38 +2,59 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) 
     {
-        int n=nums.size();
-        vector<int>prefixs(n);
-        prefixs[0]=nums[0];
-        int ans=-1;
-        for(int i=1;i<n;i++)
-        {
-            prefixs[i]=prefixs[i-1]+nums[i];
-        }
+//         int n=nums.size();
+//         vector<int>prefixs(n);
+//         prefixs[0]=nums[0];
+//         int ans=-1;
+//         for(int i=1;i<n;i++)
+//         {
+//             prefixs[i]=prefixs[i-1]+nums[i];
+//         }
         
-       // for(int i=0;i<n;i++)
+//        // for(int i=0;i<n;i++)
         
-            if(prefixs[n-1]-prefixs[0]==0)
-            {
-                ans=0;
-                return ans;
-            }
-            // else if(i==n-1 && (prefixs[n-2]==0))
-            // {
-            //     return n-1;
-            // }
-             for(int i=1;i<n;i++)
-             {
-                 if((prefixs[n-1]-prefixs[i])==prefixs[i-1])
-                {
-                     ans=i;
-                   return ans;
-                }
-             }
+//             if(prefixs[n-1]-prefixs[0]==0)
+//             {
+//                 ans=0;
+//                 return ans;
+//             }
+//             // else if(i==n-1 && (prefixs[n-2]==0))
+//             // {
+//             //     return n-1;
+//             // }
+//              for(int i=1;i<n;i++)
+//              {
+//                  if((prefixs[n-1]-prefixs[i])==prefixs[i-1])
+//                 {
+//                      ans=i;
+//                    return ans;
+//                 }
+//              }
                 
-        return ans;    
+//         return ans;   
+        
+        
+//         }
+        int lsum=0;
+        int rsum=0;
+        int ans=-1;
+        int j;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            rsum+=nums[i];
         }
+        for(j=0;j<n;j++)
+        {
+            rsum-=nums[j];
+            if(lsum==rsum)
+            {
+                ans=j;
+                return ans; 
+            }
+            lsum+=nums[j];
+        }
+        return ans;
         
-        
-    
+    }  
 };

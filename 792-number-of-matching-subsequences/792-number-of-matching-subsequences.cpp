ecfe5c68254fbdc0d411138863868e59d4f65309
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-07-20 23:52:13 +0530

    Time: 319 ms (61.19%), Space: 48.1 MB (62.91%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,56 +1,32 @@
 class Solution {
 public:
-     int isSubSeq(string& a, string b) 
-    {
-        
-        int i, j = 0;
-        int m = a.length();
-        int n = b.length();
-        
-        for(i=0;i<m && j<n;i++)
-        {
-            if (a[i] == b[j]) 
-            {
-                j++;
-            }
-        }
-        return(j==n);
-    }
-        
     int numMatchingSubseq(string s, vector<string>& words) 
     {
-         int c = 0;
-        unordered_map<string, int>mp;
-        for (int i = 0; i < words.size(); i++)
-            mp[words[i]]++;
+        vector<vector<int>> charMap(26);
+        int n = s.length();
+        
+        for(int i = 0; i < n; i++) {
+            charMap[s[i] - 'a'].push_back(i);
+        }
+        
+        int ans = words.size();
+        
+        for(auto& word : words) {
+            int last = -1;
             
-        for (auto it = mp.begin(); it != mp.end(); it++) 
-        {
-            if (isSubSeq(s, it->first))
-            {
-                c += it->second;
+            for(char c : word) {
+                auto& cIndexes = charMap[c - 'a'];
+                auto it = upper_bound(cIndexes.begin(), cIndexes.end(), last);
+                if(it == cIndexes.end()) {
+                    ans--;
+                    break;
+                } else {
+                    last = *it;
+                }
             }
         }
         
-        return c;
-        /*int i,j=0;
-        int m=s.length();
-        int c=0;
-        for(int k=0;k<words.size();k++)
-        {
-            int n=words[k].length();
-            for(i=0;i<m && j<n;i++)
-            {
-                if(s[i] == words[j])
-                {
-                    j++;
-                }
-            }
-            if(j==n)
-            {
-                c+=1;
-            }
-        }
-        return c;*/
+        return ans;
+        
     }
 };

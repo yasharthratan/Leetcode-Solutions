af225cc6822a6b790175fb6d0dab58def247c489
Yasharth Tripathi <yasharthtripathi@gmail.com>
2022-10-04 03:12:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.46%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) 
+    {
+        if(n==1)
+        {
+            return 2;
+        }
+        else if(n%2==0)
+        {
+            return n;
+        }
+        else
+        {
+            return n*2;
+        }
+        
+    }
+};
